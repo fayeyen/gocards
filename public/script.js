0adb6ff809c0c7a9dbee88452ae540a8f31a7263
Yen <faye_yen@hotmail.com>
2020-11-11 14:36:02 +0000

    cut the mass

--- public/script.js
+++ public/script.js
@@ -1,47 +1,30 @@
 // Write Javascript code!
-const appDiv = document.getElementById("app");
-appDiv.innerHTML = `<h1>Yen Cards</h1>`;
+window
+.fetch('db')
+.then(response => response.json())
+.then(
+  db => ResourcesBlock({ db })
+)
 
-function ResourceItem( name) {
-  // console.log("forEach",name);
-   document.getElementById('yenblock2').innerHTML+=
-   "Block 2<p>"+name.id+""+name.title+""+name.text+"</p>"    
-  }
-  
-function ResourceList({ db }) {
-  // db.cards.forEach (arr=> {
-  //   //  console.log('name',arr.title )
-  //    ResourceItem( arr )
-  //    })
- 
-   //  or
-  for (let i = 0; i<db.cards.length; i++) {
-    console.log('title', db.cards[i].title)
-   
+function ResourcesBlock({ db }) {
+  cards = db.cards
+  Object.keys(db).length ? ResourceList({ cards}) : NoResources()
+}
+
+function ResourceList({ cards }) {
+  for (let i = 0; i<cards.length; i++) {
     document.getElementById('yenblock').innerHTML+=
-    "Yenblock1<p>"+db.cards[i].id+""+db.cards[i].title+""+db.cards[i].text+"</p>"
-    // ResourceItem( db.cards[i] )
+    "<p>"+cards[i].id+" "+ cards[i].title+" "+cards[i].text+"</p>"
   }  
-
-
-  }
+}
   
   function NoResources() {
     return `<p>No resources found</p>`
   }
   
-  function ResourcesBlock({ db }) {
-    Object.keys(db).length ? ResourceList({ db }) : NoResources()
+
   
-  }
-  
-  window
-    .fetch('db')
-    .then(response => response.json())
-    .then(
-      db => ResourcesBlock({ db })
-    )
-  
+
   
 
 
